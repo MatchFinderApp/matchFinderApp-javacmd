2bbb74203981207fe5975a64007070454e2cb75a
mhalqahtani <moahmmed221@gmail.com>
2023-11-12 14:59:37 +0300

    edit the interface add comments

--- src/matchFinderApp/subject/messageSubject.java
+++ src/matchFinderApp/subject/messageSubject.java
@@ -1,17 +1,21 @@
 package src.matchFinderApp.subject;
 
 public class messageSubject implements subject{
+//We will imlements the method at the next stage
+
+
+// Add observer object to the list of observers to be notified\
 
     @Override
     public void addSubscriber(Observer Observer) {
        
     }
-
+    // remove observer object to the list of observers
     @Override
     public void removeSubscriber(Observer Observer) {
     
     }
-
+    // notify observer
     @Override
     public void notifySubscribers(String m) {
        
--- src/matchFinderApp/subject/subject.java
+++ src/matchFinderApp/subject/subject.java
@@ -1,6 +1,9 @@
 package src.matchFinderApp.subject;
 
-public interface subject {
+//Here is the Abstract Factory Design Pattern
+
+public abstract interface subject {
+
     void addSubscriber(Observer Observer);
     void removeSubscriber(Observer Observer);
     void notifySubscribers(String m);    
